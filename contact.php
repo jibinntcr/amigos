f8e74aa9171073155a20d0349beb7b5820267768
jerry sabu <jerry@lanwaresolutions.com>
2023-03-08 15:29:01 +0530

    contact page fixed

--- contact.php
+++ contact.php
@@ -32,7 +32,7 @@
 
     <?php include('partials/header.php') ?>
     <!-- END nav -->
-    <div class="hero-wrap" style="background-image: url('images/bg_3.jpg');">
+    <div class="hero-wrap" style="background-image: url('images/bg_3.jpg'); height: 300px;">
         <div class="overlay"></div>
         <div class="container">
             <div class="row no-gutters slider-text d-flex align-itemd-center justify-content-center">
@@ -72,7 +72,7 @@
                 </div>
             </div>
             <div class="row block-9">
-                <div class="col-md-6 order-md-last d-flex">
+                <!-- <div class="col-md-6 order-md-last d-flex">
                     <form action="#" class="bg-white p-5 contact-form">
                         <div class="form-group">
                             <input type="text" class="form-control" placeholder="Your Name">
@@ -84,32 +84,17 @@
                             <input type="text" class="form-control" placeholder="Subject">
                         </div>
                         <div class="form-group">
-                            <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+                            <textarea name="" id="" cols="30" rows="7" class="form-control"
+                                placeholder="Message"></textarea>
                         </div>
                         <div class="form-group">
                             <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
                         </div>
                     </form>
-                    <!-- <div class="col-md-4 d-flex">
-                        <div class="info rounded bg-white p-4">
-                            <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
-                        </div>
-                    </div>
-                    <div class="col-md-4 d-flex">
-                        <div class="info rounded bg-white p-4">
-                            <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
-                        </div>
-                    </div>
-                    <div class="col-md-4 d-flex">
-                        <div class="info rounded bg-white p-4">
-                            <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
-                        </div>
-                    </div> -->
+                </div> -->
 
-                </div>
-
-                <div class="col-md-6 d-flex">
-                    <iframe class="bg-white" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3927.2297722343105!2d76.38687091479551!3d10.161969392740259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTDCsDA5JzQzLjEiTiA3NsKwMjMnMjAuNiJF!5e0!3m2!1sen!2sin!4v1676951660909!5m2!1sen!2sin" width="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div class="col-md-12 d-flex">
+                    <iframe class="bg-white" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3927.2297722343105!2d76.38687091479551!3d10.161969392740259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTDCsDA5JzQzLjEiTiA3NsKwMjMnMjAuNiJF!5e0!3m2!1sen!2sin!4v1676951660909!5m2!1sen!2sin" width="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
